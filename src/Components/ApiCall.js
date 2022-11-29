01885bc2e4dfb235527d1797471d9f5b9c689612
yogesh-haryana <personalyogesh352@gmail.com>
2022-11-29 15:33:18 +0530

    eslint initilized

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -6,7 +6,6 @@ import apiKey from './Apikey';
 export const ImageContext = createContext();
 function ApiCall({ children }) {
   const [data, setData] = useState([]);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const getRemoteData = (query) => {
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then((response) => { setData(response.data.photos.photo); });
